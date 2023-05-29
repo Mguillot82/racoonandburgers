221fe3aff9742dd1d286ade83ed51cd182f81ac4
antoine lafitte <antoine.lafitte99411@gmail.com>
2023-05-29 17:16:45 +0200

    Application Controller done like a pro dont try this at home
    Signed-off-by: antoine lafitte <antoine.lafitte99411@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username,
+                                                       :photo_id,
+                                                       :first_name,
+                                                       :last_name,
+                                                       :racoon,
+                                                       :balance,
+                                                       :address,
+                                                       :city,
+                                                       :created_at,
+                                                       :updated_at,
+                                                       :email,
+                                                       :encrypted_password,
+                                                       :reset_password_token,
+                                                       :reset_password_sent_at,
+                                                       :remember_created_at])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username,
+                                                              :photo_id,
+                                                              :racoon,
+                                                              :address,
+                                                              :city,
+                                                              :email])
+  end
 end
