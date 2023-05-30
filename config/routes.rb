f0df708950a80cf6453f759a1d3671d53e673ab8
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2023-05-30 15:37:12 +0200

    ongoing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, except: [:index] do
+    resources :services, only: %i[new create edit update]
+    resources :disponibilities, only:
+  end
+  resources :reviews, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
   get "/style", to: 'pages#style'
+  get "/legal", to: 'pages#legal'
+  get "/about", to: 'pages#about'
 end
