e80f73648c548e373e6f0bd818bdc09411606aae
Mguillot82 <46008427+Mguillot82@users.noreply.github.com>
2023-05-30 16:39:51 +0200

    Update disponibilities_controller.rb

--- app/controllers/disponibilities_controller.rb
+++ app/controllers/disponibilities_controller.rb
@@ -1,47 +1,62 @@
 class DisponibilitiesController < ApplicationController
+  before_action :set_racoon
+
+  # use of scope from disponibility model to query all the disponibilities of
+  # the selected racoon from disponibilities.
   def index
-    @disponibilities = Disponibility.all
+    @disponibilities = Disponibility.user_disponibilities(@racoon.id)
   end
 
-  def show
-    @disponibility = Disponibility.find(params[:id])
-  end
+  # No need to show a specific disponibility
+  #
+  # def show
+  #   @disponibility = Disponibility.find()
+  # end
 
+  # should be only available for racoons
   def new
     @disponibility = Disponibility.new
   end
 
+  # should be only available for racoons
   def create
     @disponibility = Disponibility.new(disponibility_params)
     if @disponibility.save
-      redirect_to disponibilities_path
+      redirect_to user_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
+  # should be only available for racoons
   def edit
     @disponibility = Disponibility.find(params[:id])
   end
 
+  # should be only available for racoons
   def update
     @disponibility = Disponibility.find(params[:id])
     if @disponibility.update(disponibility_params)
-      redirect_to disponibilities_path
+      redirect_to user_path
     else
       render :edit
     end
   end
 
+  # should be only available for racoons
   def destroy
     @disponibility = Disponibility.find(params[:id])
     @disponibility.destroy
-    redirect_to disponibilities_path
+    redirect_to user_path
   end
 
   private
 
   def disponibility_params
     params.require(:disponibility).permit(:date)
+  end
+
+  def set_racoon
+    @racoon = User.find(params[:racoon_id])
   end
 end
